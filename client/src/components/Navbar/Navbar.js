0e5c7f4954ccc9fc208b8502772058ec92cff2d5
Gayatri Puppala <gayatri.puppala03@gmail.com>
2023-02-23 19:01:58 -0800

    added google auth

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import { NavItemLink, NavLogo, StyledNavbar } from "./style";
-import Logo from "../../assets/fun_box_logo.jpg"
+import Logo from "../../assets/fun_box_logo.jpg";
+
+import handleSignOut from "../../pages/LoginPage.js";
 
 // A&D Drive > Archive > Big Merch Folder > Box Logos
 
@@ -7,13 +9,16 @@ const Navbar = () => {
     return (
         <StyledNavbar>
             <NavLogo to="/home">
-                <img src={Logo} alt="Fun Box Logo"/>
+                <img src={Logo} alt="Fun Box Logo" />
             </NavLogo>
             <NavItemLink to="/faq">FAQ</NavItemLink>
             <NavItemLink to="/booth">Booth</NavItemLink>
-            <NavItemLink to="/" logout="true">Log Out</NavItemLink>
+            {/* <NavItemLink to "/" onClick={(e) => handleSignOut(e)}>Sign Out</NavItemLink> */}
+            <NavItemLink to="/" logout="true">
+                Log Out
+            </NavItemLink>
         </StyledNavbar>
-    )
-}
+    );
+};
 
 export default Navbar;
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,13 +4,94 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 import { LoginContainer } from "../components/Containers";
 
+//import Login from "../components/GAuth/gauth_login.js";
+//import Logout from "../components/GAuth/gauth_logout.js";
+
+import { useEffect, useState } from "react";
+import { gapi } from "gapi-script";
+import jwt_decode from "jwt-decode";
+
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({});
+
+    function handleCallbackResponse(response) {
+        console.log("Encoded JWT ID token:" + response.credential);
+        var userObject = jwt_decode(response.credential);
+        console.log(userObject);
+        setUser(userObject);
+        document.getElementById("signInDiv").hidden = true;
+    }
+
+    function handleSignOut(event) {
+        setUser({});
+        document.getElementById("signInDiv").hidden = false;
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id:
+                "163518163644-2ga8bmdn34uautsdm8btcrqi1g56m3hu.apps.googleusercontent.com",
+            callback: handleCallbackResponse,
+        });
+
+        google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+            theme: "outline",
+            size: "large",
+        });
+        // function start() {
+        //     gapi.client.init({
+        //         client_id: client_id,
+        //         scope: "",
+        //     });
+        // }
+
+        // gapi.load("client:auth2", start);
+    }, []);
+
+    //login button but if have user, show logout
+
     return (
         <Page lightblue>
             <LoginContainer>
                 <h1>Secret DJ Panel</h1>
                 <h3>Please log in with your account</h3>
-                <form onSubmit={(e) => e.preventDefault()}>
+
+                <div id="signInDiv"></div>
+                {
+                    //if user is logged in, show sign out button
+                    Object.keys(user).length != 0 && (
+                            <Button onClick={(e) => handleSignOut(e)}>
+                                Sign Out
+                            </Button>
+                        ) &&
+                        navigate("/home")
+                }
+
+                {/* <Button
+                    type="submit"
+                    onClick={() => {
+                        navigate("/home");
+                        // if (currentUserEmail() != null) {
+                        //     navigate("/Profile");
+                        // } else {
+                        //     login().then((result) => {
+                        //         if (result) {
+                        //             navigate("/Profile");
+                        //         }
+                        //     });
+                        // }
+                    }}
+                >
+                    Log in
+                    </Button>
+                 */}
+
+                {/* <form onSubmit={(e) => e.preventDefault()}>
                     <InputGroup>
                         <label htmlFor="login-email">Email Address</label>
                         <Input type="text" placeholder="name@email.com" id="login-email"/>
@@ -27,10 +108,10 @@ const LoginPage = () => {
                     <UnderlinedLink to="forgot-password">Forgot password?</UnderlinedLink>
                     <Spacer width="125px"/>
                     <UnderlinedLink to="signup">Request an Account</UnderlinedLink>
-                </span>
+                </span> */}
             </LoginContainer>
         </Page>
     );
-}
+};
 
 export default LoginPage;
